0a034263afd3519977db15e8291e623d35c744d2
AshfatAbegh <mdabegh50@gmail.com>
2022-09-15 19:02:37 +0600

    learning node

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const users = ["Araf","Rahim","Saif"];
+
 /*function rootCall(req, res){
     res.send('Thank you');
 }*/
@@ -11,7 +13,15 @@ const app = express();
 }*/
 
 app.get('/', (req, res) =>{
-    res.send("Node is running");
+    res.send("Learning Node");
 });
+app.get('/fruits/banana', (req,res) => {
+    res.send({fruit:'banana', quantity:1000, price:10000});
+})
+app.get('/users/:id', (req,res) =>{
+    const id = req.params.id;
+    const name =  users[id]; 
+    res.send({id, name});
+})
 
 app.listen(4000, () => console.log("Listening to port 4000"));
